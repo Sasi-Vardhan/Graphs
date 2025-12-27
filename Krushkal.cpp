369351b38cfa4202503fa3291fe81f7daf5a5280
Sasi_Vardhan_Vemuri <163669536+Sasi-Vardhan@users.noreply.github.com>
2025-12-27 13:55:12 +0530

    Create Krushkal.cpp

---
+++ Krushkal.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    int findParent(vector<int>& parents, int node) {
+        if (parents[node] == node) return node;
+        return parents[node] = findParent(parents, parents[node]);
+    }
+    
+    int unions(vector<int>& parents, vector<int>& rank, int a, int b) {
+        int rootA = findParent(parents, a);
+        int rootB = findParent(parents, b);
+        
+        if (rootA == rootB) return -1;
+        
+        if (rank[rootA] < rank[rootB]) {
+            parents[rootA] = rootB;
+        } else if (rank[rootA] > rank[rootB]) {
+            parents[rootB] = rootA;
+        } else {
+            parents[rootB] = rootA;
+            rank[rootA]++;
+        }
+        return 1;
+    }
+
+    int spanningTree(int V, vector<vector<int>>& edges) {
+
+        // parents and rank sized to V
+        vector<int> parents(V);
+        vector<int> rank(V, 0);
+
+        // initialize DSU
+        for (int i = 0; i < V; i++)
+            parents[i] = i;
+
+        // swap to {w, u, v}
+        for (int i = 0; i < edges.size(); i++) {
+            int u = edges[i][0];
+            int v = edges[i][1];
+            int w = edges[i][2];
+            edges[i] = {w, u, v};
+        }
+
+        // sort by weight (now index 0)
+        sort(edges.begin(), edges.end());
+
+        int ans = 0;
+
+        for (int i = 0; i < edges.size(); i++) {
+            int w = edges[i][0];
+            int u = edges[i][1];
+            int v = edges[i][2];
+
+            if (unions(parents, rank, u, v) == 1)
+                ans += w;
+        }
+
+        return ans;
+    }
+};
